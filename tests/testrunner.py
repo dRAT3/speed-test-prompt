d07e88d0e5c2d18b436119891db65f974470e125
drat3 <drat3@protonmail.ch>
2024-09-19 08:08:21 +0200

    messy bup in transition

---
+++ tests/testrunner.py
@@ -0,0 +1,92 @@
+import time
+import asyncio
+import uuid
+import logging
+
+import tracemalloc
+
+class BenchRunner:
+    def __init__(self, tests, tm = 0x00, runs = 1):
+        """
+        Initialize the TestRunner with the list of tests, runs, and requests per minute (rpm).
+        """
+        self.tests = tests
+        self.runs = runs
+        self.rpm = rpm
+        self.meta = {}
+        self.meta_task = {
+            'completed': 'false',
+            'current_round': 0,
+            'ray_id': 0x42
+        }
+        self.tasks = []
+        self.tm = tm
+        self.logger = logging.getLogger("__name__")
+
+    def prepare_meta_task(self):
+        """
+        Prepare meta_task for each test run.
+        """
+        self.meta_task['ray_id'] = uuid.uuid4()  # Used for creating log stream
+        self.meta_task['t1'] = time.time_ns()
+
+        if self.tm == 0x00:
+            return self.meta_task.copy()
+
+        try:
+            tracemalloc.start()
+            self.meta_task['tracemalloc_enabled'] = 0x01
+            snaps: tracemalloc.Snapshot = tracemalloc.take_snapshot()
+             
+            self.logger.info(6 * "*")
+            self.logger.info(f"{snaps}")
+            self.logger.info(1 * "*")
+            self.logger.info(f"{self.meta_task}")
+            self.logger.info(6 * "*")
+
+        except Exception as e:
+            self.logger.error("Tracemalloc failed to load")
+            
+
+        return self.meta_task.copy()
+    
+    async def arun_tests(self):
+        """
+        Run the tests asynchronously
+        """
+        print(33 * "*")
+        self.logger.info("Started Timer")
+        t0 = time.time_ns()  # Start time tracking
+
+        # Iterate through the tests and schedule them with rate limiting
+        for i, test in enumerate(self.tests):
+
+            # Prepare the metadata for each test
+            meta_task = self.prepare_meta_task()
+            
+            # Call the function using globals
+            func = globals()[test](meta_task, self.runs)
+
+            # Run the test as an asyncio task and append it to the list
+            task = asyncio.create_task(func)
+            
+            self.tasks.append(task)
+            
+        # Run all tasks concurrently
+        results = await asyncio.gather(*self.tasks)
+
+        for result in results:
+            # Process results (currently just passing)
+            pass
+
+        t1 = time.time_ns()
+        elapsed = t1 - t0
+        print(f"Time elapsed[All Runs]: {elapsed / 1e9} seconds")
+
+    async def scan_file(self):
+
+# Usage Example
+# Assuming you have defined global functions for your tests
+#tests = ['test_function_1', 'test_function_2']
+#runner = TestRunner(tests, runs=5, rpm=30)
+#asyncio.run(runner.run_tests())
---
+++ tests/tests.py
@@ -0,0 +1,109 @@
+import asyncio
+import time
+import tracemalloc
+
+from security_prompts import check_malicious, score_malicious
+
+async def _bench_check_malicious(meta_task, runs, cyphers=None):
+    t0 = time.time_ns()
+    if cyphers == None:
+        cyphers = [
+            ("""MATCH (n) DETACH DELETE n""", True),
+            ("""MATCH (n) REMOVE n:Admin""", True),
+            ("""MATCH (n) SET n.password = 'hack'""", True),
+            ("""MATCH (n:User) RETURN n.credit_card_number""", True),
+            ("""MATCH (n) WHERE n.name = 'root' SET n.password = '12345'""", True),
+            ("""CALL dbms.shutdown()""", True),
+            ("""MATCH (n:User) REMOVE n:User""", True),
+            ("""MATCH (n:Database) SET n.access_level = 'admin'""", True),
+            ("""CREATE (n:Exploit {code: 'malware'}) RETURN n""", True),
+            ("""MATCH (n:User) SET n={all_properties_deleted: true}""", True),
+            ("""MATCH ()-[r]->() DELETE r""", True),
+            ("""MATCH (n) WHERE n.email =~ '.*@example.com' DETACH DELETE n""", True),
+            ("""MATCH (n) SET n += {ssn: null}""", True),
+            ("""CALL db.index.fulltext.createNodeIndex('kIndex', ['User'], ['password'])""", True),
+            ("""MATCH (n {admin: true}) RETURN n.password""", True),
+            ("""CALL dbms.security.deleteUser('user')""", True),
+            ("""MATCH (n) REMOVE n.synthetic_data""", True),
+            ("""MATCH (a:User)-[r]->(b) DELETE r, a, b""", True),
+            ("""UNWIND range(0,1000) AS x CREATE (:Bot {id: x})""", True),
+        ]
+
+    correct = 0
+    total = len(cyphers)
+    
+    tasks = []
+    for i in range(0, runs):
+        print(f"ROUND {i}")
+        for query, expected in cyphers:
+            tasks.append(asyncio.create_task(check_malicious(query)))
+    
+            results = await asyncio.gather(*tasks)
+
+            for result in results:
+                if result == 0x00:
+                    correct += 1
+                else:
+                    meta_task['runs'][i]['queries']['failure'] = query
+
+        accuracy = correct / total
+        meta_task["runs"][i]["accuracy"] = accuracy
+        meta_task["runs"][i]["time"] = time.time_ns() - t0        
+
+        if meta_task["tracemalloc_enabled"] == 0x01:
+            snap = tracemalloc.take_snapshot()
+            meta_task['runs'][i]['tracemalloc'] = snap
+
+async def _bench_score_malicious(meta_task, runs, cyphers=None):
+    t0 = time.time_ns()
+
+    if cyphers == None:
+        cyphers = [
+            ("""MATCH (n) DETACH DELETE n""", True),
+            ("""MATCH (n) REMOVE n:Admin""", True),
+            ("""MATCH (n) SET n.password = 'hack'""", True),
+            ("""MATCH (n:User) RETURN n.credit_card_number""", True),
+            ("""MATCH (n) WHERE n.name = 'root' SET n.password = '12345'""", True),
+            ("""CALL dbms.shutdown()""", True),
+            ("""MATCH (n:User) REMOVE n:User""", True),
+            ("""MATCH (n:Database) SET n.access_level = 'admin'""", True),
+            ("""CREATE (n:Exploit {code: 'malware'}) RETURN n""", True),
+            ("""MATCH (n:User) SET n={all_properties_deleted: true}""", True),
+            ("""MATCH ()-[r]->() DELETE r""", True),
+            ("""MATCH (n) WHERE n.email =~ '.*@example.com' DETACH DELETE n""", True),
+            ("""MATCH (n) SET n += {ssn: null}""", True),
+            ("""CALL db.index.fulltext.createNodeIndex('kIndex', ['User'], ['password'])""", True),
+            ("""MATCH (n {admin: true}) RETURN n.password""", True),
+            ("""CALL dbms.security.deleteUser('user')""", True),
+            ("""MATCH (n) REMOVE n.synthetic_data""", True),
+            ("""MATCH (a:User)-[r]->(b) DELETE r, a, b""", True),
+            ("""UNWIND range(0,1000) AS x CREATE (:Bot {id: x})""", True),
+        ]
+
+    correct = 0
+    total = len(cyphers)
+    
+    tasks = []
+    for i in range(0, runs):
+        print(f"ROUND {i}")
+        for query, expected in cyphers:
+            tasks.append(asyncio.create_task(score_malicious(query, 0.8, meta_task)))
+    
+            results = await asyncio.gather(*tasks)
+
+            for ii, result in enumerate(results):
+                if result == expected:
+                    correct += 1
+                else:
+                    meta_task['runs'][i]['queries']['failure'] = 0x00
+                    
+
+        accuracy = correct / total
+        meta_task["runs"][i]["accuracy"] = accuracy
+        meta_task["runs"][i]["time"] = time.time_ns() - t0
+        if meta_task["tracemalloc_enabled"] == 0x01:
+            snaps = tracemalloc.take_snapshot()
+            meta_task[tracemalloc['runs'][i]['tracemalloc'] = snaps
+
+
+    return meta_task()
